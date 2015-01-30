b31f61cfb4daf25e8d88ac9d703783299868aea4
Joe Sweeney <SweeneyJohnJoseph@gmail.com>
2015-01-30 14:45:36 -0500

    Added extra testing features

--- src/TestSort.java
+++ src/TestSort.java
@@ -1,8 +1,21 @@
+import java.util.Random;
+
 public class TestSort {
 
 	public static void main(String[] args) {
-		float[] ints = {1,3,5,14,0,9,3,1,413};
-		BubbleSort bubble = new BubbleSort(ints, false);
+		float[] ints = new float[500];
+		
+		Random rand = new Random();
+		int x;
+		
+		for (int i = 0; i < 500; i++) {
+		  x = rand.nextInt(500);
+		  ints[i] = x;
+		}
+		
+		System.out.println("Finished filling!!!");
+		
+		BubbleSort bubble = new BubbleSort(ints, true);
 		
 		System.out.println(bubble);
 	}
